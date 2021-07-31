040ba08ad4e1a619a63645796f2c56fbc31cc6e9
SnehalBorade2032 <88225880+SnehalBorade2032@users.noreply.github.com>
2021-07-31 13:00:51 +0530

    Delete main.py
    i want to delete

--- main.py
+++
@@ -1,8 +0,0 @@
-
-import csv
-
-tab_in = csv.reader(open('EQ010114.tsv'), dialect=csv.excel_tab)
-comma_out = csv.writer(open('EQ10114.csv', 'w'), dialect=csv.excel)
-
-for row in tab_in:
-    comma_out.writerow(row)
